3425f57d09f0d80da691608d11f113d354fda9d2
Alexander Kudryavtsev <aakudr2k3@gmail.com>
2022-05-23 16:42:42 +0300

    finish guiv3, add analyser v2 file

--- Scripts/guiv3.py
+++ Scripts/guiv3.py
@@ -1,11 +1,19 @@
 import cv2
 import time
 import os
+from tkinter import filedialog
 
 title_window = 'window'
 thresh_value = 0
 slider_max = 255
 file_extension = 'tvalue'
+
+def browse():
+    filename = filedialog.askdirectory()
+    print(filename)
+    return filename
+
+folder_path = browse()
 
 def on_trackbar(val):
     global thresh_value
@@ -18,24 +26,30 @@ def save_file(path, val, ext):
     f.write(str(val))
     f.close()
 
-PATH = "/home/lab/Downloads/MECLO_C_M_26_83.avi.avi"
-cap = cv2.VideoCapture(PATH)
+for filename in os.listdir(folder_path):
+    f = os.path.join(folder_path, filename)
+    if os.path.isfile(f) and f[-4:] == ".avi":
+        if filename + '.' + file_extension in os.listdir(folder_path):
+            continue
+        print(f)
+        cap = cv2.VideoCapture(f)
 
-cv2.namedWindow(title_window)
-trackbar_name = 'threshold'
-cv2.createTrackbar(trackbar_name, title_window , 0, slider_max, on_trackbar)
+        cv2.namedWindow(title_window)
+        trackbar_name = 'threshold'
+        cv2.createTrackbar(trackbar_name, title_window , 0, slider_max, on_trackbar)
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 3)
-    
-    ret, thresh = cv2.threshold(gray, thresh_value, 255, cv2.THRESH_BINARY_INV)
-    print(thresh_value)
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.medianBlur(gray, 3)
+            
+            ret, thresh = cv2.threshold(gray, thresh_value, 255, cv2.THRESH_BINARY_INV)
+            print(thresh_value)
 
-    cv2.imshow('title_window', thresh)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    time.sleep(0.015)
+            cv2.imshow('title_window', thresh)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            time.sleep(0.015)
 
-save_file(PATH, thresh_value, file_extension)
+        save_file(f, thresh_value, file_extension)
+        cv2.destroyAllWindows()
