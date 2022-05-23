b19005d41e069c4e30646e0b9351bc3a8f61ed77
Alexander Kudryavtsev <aakudr2k3@gmail.com>
2022-05-23 17:30:27 +0300

    fix path

--- Scripts/analyse-video-v2.py
+++ Scripts/analyse-video-v2.py
@@ -1,6 +1,6 @@
 import cv2
 
-PATH = "home/lab/Downloads/MECLO_C_M_26_83.avi.avi"
+PATH = "/home/lab/Downloads/MECLO_C_M_26_83.avi.avi"
 
 def init_capture(path):
     cap = cv2.VideoCapture(path)
@@ -11,11 +11,11 @@ def read_thresh_frame(cap, thresh_value):
     ret, thresh = cv2.threshold(frame, thresh_value, 255, cv2.THRESH_BINARY_INV)
     return thresh
 
-while(cap.isOpened()):
-    c = init_capture(PATH)
-    while c.isOpened():
-        frame = read_thresh_frame(c, 110)
-        cv2.imshow(PATH, frame)
-        
+c = init_capture(PATH)
+
+while c.isOpened():
+    frame = read_thresh_frame(c, 110)
+    cv2.imshow(PATH, frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
